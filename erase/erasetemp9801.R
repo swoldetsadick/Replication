247eb5042f6ca85001074c15b9988607959e0fce
swoldetsadick <swoldetsadick@ineteconomics.org>
2014-10-02 02:09:18 +0200

    message

--- erase/erasetemp9801.R
+++ erase/erasetemp9801.R
@@ -7,7 +7,7 @@ temp98 <- csuro98[,c(31, 35, 43, 45, 60)]
 temp98 <- temp98[order(temp98$identif),]
 
 csur01 <- read.table("~/Downloads/Desktop/Replication/erase/erasecsur01.txt", header=TRUE, quote="\"")
-csuro01 <- subset(csur01, year(csur01$anno) ==2000)
+csuro01 <- subset(csur01, year(csur01$anno) == 2000)
 
 temp01 <- csuro01[,c(40, 41, 43, 45, 60)]
 temp01 <- temp01[order(temp01$identif),]
@@ -17,21 +17,38 @@ merge2 <- merge(temp98, temp01, by = c("identif"), all = TRUE)
 merge2$eta98n <- merge2$eta01n - 3
 merge2$diff <- merge2$eta01 - merge2$eta98
 
-z <- ifelse(merge2$diff == 3, 1, 0)
+z <- ifelse(merge2$diff == 3 , 1, 0)
+merge2$z <- ifelse(is.na(z), 0, z)
+
 y <- ifelse(merge2$eta01 == merge2$eta01n, 0, 1)
-x <- ifelse(!is.na(merge2$eta98n), 0, 1)
+
+merge2$x <- ifelse(!is.na(merge2$eta98n), 0, 1)
+
 w <- ifelse(merge2$eta98n >= 0, 0, 1)
 v <- z + y + x + w
 
-merge2$eta98 <- ifelse(w, merge2$eta98 == merge2$eta98n, merge2$eta98)
+merge2$eta98 <- ifelse(w == 0, merge2$eta98n, merge2$eta98)
 
-u <- ifelse(merge2$diff == 3, 1, 0)
-t <- ifelse(merge2$eta98 == merge2$eta98n, 0, 1)
+u <- ifelse(merge2$diff == 3 | is.na(merge2$diff), 1, 0)
+t <- ifelse(is.na(merge2$eta98)|is.na(merge2$eta98n), 1, ifelse(merge2$eta98 == merge2$eta98n, 0, 1))
 s <- ifelse(!is.na(merge2$eta01n), 0, 1)
-r <- ifelse(merge2$eta01n >= 0, 0, 1)
+r <- ifelse(merge2$eta01n < 0 | is.na(merge2$eta01n), 1, 0)
 q <- z + y + x + w
 
-merge2$eta01 <- ifelse(q, merge2$eta01 == merge2$eta01n, merge2$eta01)
+merge2$eta01 <- ifelse(q == 0, merge2$eta01n, merge2$eta01)
+
+#merge2$eta98 <- ifelse(merge2$diff != 3 & merge2$eta01 == merge2$eta01n & !is.na(merge2$eta98n) & merge2$eta98n >= 0, merge2$eta98n, merge2$eta98)
+#merge2$eta01 <- ifelse(merge2$diff != 3 & merge2$eta98 == merge2$eta98n & !is.na(merge2$eta01n) & merge2$eta01n >= 0, merge2$eta01n, merge2$eta01)
+
+merge2$eta98 <- ifelse(is.na(merge2$eta98) & merge2$eta98 < 0 & !merge2$eta01, merge2$eta01 - 3, merge2$eta98)
+merge2$eta01 <- ifelse(is.na(merge2$eta01) & merge2$eta01 < 0 & !merge2$eta98, merge2$eta98 + 3, merge2$eta01)
+
+##p <- ifelse(is.na(merge2$eta98), 1, ifelse(merge2$eta98 < 0, 2, 3))
+##o <- ifelse(is.na(merge2$eta01), 10, ifelse(merge2$eta01 < 0, 20, 30))
+##n <- p + o
+
+##merge2$eta98 <- ifelse(n == 21|n == 22|n == 31|n == 32, merge2$eta01 - 3, merge2$eta98)
+##merge2$eta01 <- ifelse(n == 12|n == 13|n == 22|n == 23, merge2$eta98 + 3, merge2$eta01)
 
 #merge2$eta98 <- ifelse((merge2$diff !=3|is.na(merge2$diff)) & (merge2$eta01== merge2$eta01n| is.na(merge2$eta01)| is.na(merge2$eta01n)) & !(is.na(merge2$eta98n))& merge2$eta98n>=0, merge2$eta98n, merge2$eta98)
 #merge2$eta01 <- ifelse((merge2$diff !=3|is.na(merge2$diff)) & (merge2$eta98== merge2$eta98n|(is.na(merge2$eta98)| is.na(merge2$eta98n))) & !(is.na(merge2$eta01n))& merge2$eta01n>=0, merge2$eta01n, merge2$eta01)
