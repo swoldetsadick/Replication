0688455aac9a4a732f4ed8e775aa3a49d1242077
swoldetsadick <swoldetsadick@ineteconomics.org>
2014-10-06 05:47:30 +0200

    message

--- erase/erasetempstime.R
+++ erase/erasetempstime.R
@@ -1,11 +1,11 @@
-setwd("C:/Users/anton_000/Documents/Downloads/Desktop/Replication/erase")
+#setwd("C:/Users/anton_000/Documents/Downloads/Desktop/Replication/erase")
 
-csur92 <- read.table("./temp92.txt", header = TRUE)
-csur95 <- read.table("./temp95.txt", header = TRUE)
-csur98 <- read.table("./temp98.txt", header = TRUE)
-csur01 <- read.table("./temp01.txt", header = TRUE)
+#csur92 <- read.table("./temp92.txt", header = TRUE)
+#csur95 <- read.table("./temp95.txt", header = TRUE)
+#csur98 <- read.table("./temp98.txt", header = TRUE)
+#csur01 <- read.table("./temp01.txt", header = TRUE)
 
-### Generating data set
+### Generating Data Set
 
 library(lubridate)
 
@@ -29,20 +29,21 @@ csur01$eta <- ifelse(!is.na(csur01$eta01) & year(csur01$anno) == 1999, csur01$et
 csur01$eta <- ifelse(!is.na(csur01$eta01) & year(csur01$anno) == 1998, csur01$eta - 2, csur01$eta)
 csur01 <- csur01 [order(csur01$identif, csur01$anno),]
 
-write.table(csur92, "./csur92.txt", row.names=FALSE)
-write.table(csur95, "./csur95.txt", row.names=FALSE)
-write.table(csur98, "./csur98.txt", row.names=FALSE)
-write.table(csur01, "./csur01.txt", row.names=FALSE)
+#write.table(csur92, "./csur92.txt", row.names=FALSE)
+#write.table(csur95, "./csur95.txt", row.names=FALSE)
+#write.table(csur98, "./csur98.txt", row.names=FALSE)
+#write.table(csur01, "./csur01.txt", row.names=FALSE)
 
-csur92 <- read.table("./csur92.txt", header = TRUE)
-csur95 <- read.table("./csur95.txt", header = TRUE)
-csur98 <- read.table("./csur98.txt", header = TRUE)
-csur01 <- read.table("./csur01.txt", header = TRUE)
+#csur92 <- read.table("./csur92.txt", header = TRUE)
+#csur95 <- read.table("./csur95.txt", header = TRUE)
+#csur98 <- read.table("./csur98.txt", header = TRUE)
+#csur01 <- read.table("./csur01.txt", header = TRUE)
 
 merge7 <- merge(merge(csur92, csur95, all.x = TRUE, all.y = TRUE), merge(csur98, csur01, all.x = TRUE, all.y = TRUE), all.x = TRUE, all.y = TRUE)
 merge7 <- merge7[order(merge7$identif, merge7$anno),]
 
-### Generating lags
+### Generating LEAD and LAG
+
 library(lubridate)
 merge7 <-  within(merge7, { 
         detab <- ave(eta, identif, FUN = function(x) c(NA, diff(x))) 
@@ -67,35 +68,22 @@ merge7 <-  within(merge7, {
 merge7$dip <- ifelse(year(merge7$anno)==1989,merge7$dip89,ifelse(year(merge7$anno)==1990,merge7$dip90,ifelse(year(merge7$anno)==1991,merge7$dip91,ifelse(year(merge7$anno)==1992,merge7$dip92,ifelse(year(merge7$anno)==1993,merge7$dip93,ifelse(year(merge7$anno)==1994,merge7$dip94,ifelse(year(merge7$anno)==1995,merge7$dip95,ifelse(year(merge7$anno)==1996,merge7$dip96,ifelse(year(merge7$anno)==1997,merge7$dip97,ifelse(year(merge7$anno)==1998,merge7$dip98,ifelse(year(merge7$anno)==1999,merge7$dip99,merge7$dip00)))))))))))
 
 merge7$identif <- as.factor(merge7$identif)
+merge7$id <- merge7$identif
 
-# Creating identif LAG by identif
-E <- data.frame(as.factor(merge7$identif))
-G <- data.frame(rbind(as.character(NA), E))
-G <- data.frame(G[-nrow(G),])
-H <- data.frame(cbind(E, G))
-colnames(H) <- c("nlg", "lg1")
-H$rl <- ifelse(H$nlg == H$lg1, as.character(H$nlg), NA)
-
-# Creating identif LEAD by identif
-I <- data.frame(rbind(E, as.character(NA)))
-I <- data.frame(I[-1,])
-J <- data.frame(cbind(E, I))
-colnames(J) <- c("nad", "ad1")
-J$ra <- ifelse(J$nad == J$ad1, as.character(J$nad), NA)
-
-# identif LAG is called id1 and LEAD id3 (by identif)
-merge7$id1 <- H$rl
-merge7$id3 <- J$ra
-
-# creating dip1, dip2 and dip3 variables which represent number of firms by identif
-# lagged one time, two times and leaded one time respectively
+# creating dip1, dip2 and dip3 variables which represent number of employees of 
+# firms by identif lagged one time, two times and leaded one time respectively
+# Also variables id1 and id3 lagged and lead one time variable identif
 suppressMessages(library(data.table))
 merge7 <- data.table(merge7, key = "identif")
+merge7[,c("id1") := list(c(NA, dip[-.N])), by = identif]
+
 merge7[,c("dip1") := list(c(NA, dip[-.N])), by = identif]
 merge7[,c("dip2") := list(c(NA, dip1[-.N])), by = identif]
 
 merge7 <- merge7[order(merge7$identif, merge7$anno, decreasing = TRUE)]
+merge7[,c("id3") := list(c(NA, dip[-.N])), by = identif]
 merge7[,c("dip3") := list(c(NA, dip[-.N])), by = identif]
+
 merge7 <- merge7[order(merge7$identif, merge7$anno, decreasing = FALSE)]
 
 # Interpolating possible missing values of number of employees in firm
@@ -112,18 +100,34 @@ suppressWarnings(merge7$ldip <- ifelse(is.infinite(log(merge7$dip)), NA, log(mer
 suppressWarnings(merge7$ldip1 <- ifelse(is.infinite(log(merge7$dip1)), NA, log(merge7$dip1)))
 suppressWarnings(merge7$ldip2 <- ifelse(is.infinite(log(merge7$dip2)), NA, log(merge7$dip2)))
 
-merge7$id <- merge7$identif
-
-lg <- function(x)c(NA,x[1:(length(x)-1)])
-ld <- function(x)c(NA,x[1:(length(x)+1)])
-merge7[, lid := lg(id), by = c("identif")]
-merge7[, vid := ld(id), by = c("identif")]
-
 merge7 <- data.frame(merge7)
 
+# Erasing un-needed variables
+merge7$id <- NULL
 merge7$id1 <- NULL
 merge7$id3 <- NULL
 merge7$dip1 <- NULL
 merge7$dip2 <- NULL
 merge7$dip3 <- NULL
+
+### Generating Dummies
+merge7$duage <- ifelse(merge7$eta<7,0,ifelse(6<merge7$eta&16>merge7$eta,1,ifelse(15<merge7$eta&26>merge7$eta,2,ifelse(25<merge7$eta&51>merge7$eta,3,ifelse(50<merge7$eta|is.na(merge7$eta),4,NA)))))
+ugo <- 7
+merge7$dupico <- ifelse(merge7$dip < 50, 1, 0)
+merge7$razdy <-ifelse(merge7$razd == 1 & merge7$eta < ugo, 1, ifelse(is.na(merge7$razd), NA, 0))
+merge7$dimmy <- ifelse(merge7$dim == 1 & merge7$eta < ugo, 1, 0)
+merge7$dofuy <- ifelse(merge7$duofut == 1 & merge7$eta < ugo, 1, 0)
+merge7$razdp <- ifelse(merge7$razd == 1 & merge7$dupico == 1, 1, ifelse(is.na(merge7$razd), NA, 0))
+merge7$dimmp <- ifelse(merge7$dimm == 1 & merge7$dupico == 1, 1, 0)
+merge7$dofup <- ifelse(merge7$duofut == 1 & merge7$dupico == 1, 1, 0)
+
+#Total Assets
+
+merge7$ato <- (merge7$aco + merge7$aim)
+merge7$dudex <- ifelse(merge7$desi == 1, 1, 0)
+merge7$ldim <- log(merge7$ato)
+merge7$dudexy <- ifelse(merge7$dudex == 1 & merge7$eta < ugo, 1, ifelse(is.na(merge7$desi), NA, 0))
+merge7$dudexp <- ifelse(merge7$dudex == 1 & merge7$dupico == 1, 1, ifelse(is.na(merge7$desi), NA, 0))
+
+tempstime <- merge7[order(merge7$identif, merge7$anno), ]
 
